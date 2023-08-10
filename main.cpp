f221615618a7c4c556dbdf16b367d958b5a1d582
maciulex <41756057+maciulex@users.noreply.github.com>
2023-08-10 12:19:23 +0200

    display working

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+#include "SH1106.cpp"
+
+int main() {
+    stdio_init_all();
+    printf("start");
+    sleep_ms(1000);
+    printf("start\n");
+    sleep_ms(1000);
+    printf("start\n");
+    sleep_ms(1000);
+    printf("start\n");
+
+    SH1106::init(true);
+
+    while(true);
+    return 0;
+}
